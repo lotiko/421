0836d57694d3d06d6c7d82208645cc587cafe776
lotiko <l.despres@netc.fr>
2021-03-07 11:13:34 +0100

    add function draw()

--- javascripts/modules/game.mjs
+++ javascripts/modules/game.mjs
@@ -67,9 +67,59 @@ rollDicesBtn.addEventListener("click", function (ev) {
 ///////// CANVAS UTILS
 /// Thanks to Tamas Berki and its code pen https://codepen.io/dzsobacsi/pen/pjxEOK/
 /// that inspired me
-// const diceColor = "#f0ea";
-// const dotColor = "#0000";
-// const diceTest = dicesCombinaisonCanvasByPlayer.p1.d1;
+const diceColor = "black";
+const dotColor = "#ffff";
+const diceTest = dicesCombinaisonCanvasByPlayer.p1.d1;
+const dots = [];
+const size = 35;
+//define dot locations
+const padding = 0.25;
+let x, y;
+x = padding * size;
+y = padding * size;
+dots.push({ x: x, y: y });
+y = size * 0.5;
+dots.push({ x: x, y: y });
+y = size * (1 - padding);
+dots.push({ x: x, y: y });
+x = size * 0.5;
+y = size * 0.5;
+dots.push({ x: x, y: y });
+x = size * (1 - padding);
+y = padding * size;
+dots.push({ x: x, y: y });
+y = size * 0.5;
+dots.push({ x: x, y: y });
+y = size * (1 - padding);
+dots.push({ x: x, y: y });
+function draw(value) {
+  if (diceTest.getContext) {
+    var ctx = diceTest.getContext("2d");
+    console.log(ctx);
+    let dotsToDraw;
+    if (value == 1) dotsToDraw = [3];
+    else if (value == 2) dotsToDraw = [0, 6];
+    else if (value == 3) dotsToDraw = [0, 3, 6];
+    else if (value == 4) dotsToDraw = [0, 2, 4, 6];
+    else if (value == 5) dotsToDraw = [0, 2, 3, 4, 6];
+    else if (value == 6) dotsToDraw = [0, 1, 2, 4, 5, 6];
+    else console.log("Dice value shall be between 1 and 6");
+    roundRect(ctx, 0, 0, size, size, 8, true);
+    for (let i = 0; i < dotsToDraw.length; i++) {
+      ctx.fillStyle = dotColor;
+      ctx.strokeStyle = dotColor;
+      ctx.save();
+      ctx.beginPath();
+      const j = dotsToDraw[i];
+      ctx.arc(dots[j].x, dots[j].y, size * 0.07, 0, 2 * Math.PI);
+      ctx.fill();
+      ctx.stroke();
+
+      ctx.closePath();
+    }
+  }
+}
+draw(6);
 // let ctx = diceTest.getContext("2d");
 // console.log(ctx);
 // drawDice(ctx, diceTest.width, diceTest.width, diceTest.width, 1, diceColor, dotColor);
@@ -91,36 +141,36 @@ rollDicesBtn.addEventListener("click", function (ev) {
 //   ctx.translate(x, y);
 //   roundRect(ctx, 0, 0, size, size, size * 0.1, true, false);
 
-//   //define dot locations
-//   const padding = 0.25;
-//   const x, y;
-//   x = padding * size;
-//   y = padding * size;
-//   dots.push({ x: x, y: y });
-//   y = size * 0.5;
-//   dots.push({ x: x, y: y });
-//   y = size * (1 - padding);
-//   dots.push({ x: x, y: y });
-//   x = size * 0.5;
-//   y = size * 0.5;
-//   dots.push({ x: x, y: y });
-//   x = size * (1 - padding);
-//   y = padding * size;
-//   dots.push({ x: x, y: y });
-//   y = size * 0.5;
-//   dots.push({ x: x, y: y });
-//   y = size * (1 - padding);
-//   dots.push({ x: x, y: y });
+//define dot locations
+// const padding = 0.25;
+// const x, y;
+// x = padding * size;
+// y = padding * size;
+// dots.push({ x: x, y: y });
+// y = size * 0.5;
+// dots.push({ x: x, y: y });
+// y = size * (1 - padding);
+// dots.push({ x: x, y: y });
+// x = size * 0.5;
+// y = size * 0.5;
+// dots.push({ x: x, y: y });
+// x = size * (1 - padding);
+// y = padding * size;
+// dots.push({ x: x, y: y });
+// y = size * 0.5;
+// dots.push({ x: x, y: y });
+// y = size * (1 - padding);
+// dots.push({ x: x, y: y });
 //   //for(const i=0; i<dots.length; i++) console.log(dots[i]);
 
 //   const dotsToDraw;
-//   if (value == 1) dotsToDraw = [3];
-//   else if (value == 2) dotsToDraw = [0, 6];
-//   else if (value == 3) dotsToDraw = [0, 3, 6];
-//   else if (value == 4) dotsToDraw = [0, 2, 4, 6];
-//   else if (value == 5) dotsToDraw = [0, 2, 3, 4, 6];
-//   else if (value == 6) dotsToDraw = [0, 1, 2, 4, 5, 6];
-//   else console.log("Dice value shall be between 1 and 6");
+// if (value == 1) dotsToDraw = [3];
+// else if (value == 2) dotsToDraw = [0, 6];
+// else if (value == 3) dotsToDraw = [0, 3, 6];
+// else if (value == 4) dotsToDraw = [0, 2, 4, 6];
+// else if (value == 5) dotsToDraw = [0, 2, 3, 4, 6];
+// else if (value == 6) dotsToDraw = [0, 1, 2, 4, 5, 6];
+// else console.log("Dice value shall be between 1 and 6");
 
 //   ctx.fillStyle = dotColor;
 //   for (const i = 0; i < dotsToDraw.length; i++) {
@@ -152,39 +202,39 @@ rollDicesBtn.addEventListener("click", function (ev) {
 // //  * @param {Boolean} [fill = false] Whether to fill the rectangle.
 // //  * @param {Boolean} [stroke = true] Whether to stroke the rectangle.
 // //  */
-// function roundRect(ctx, x, y, width, height, radius, fill, stroke) {
-//   if (typeof stroke == "undefined") {
-//     stroke = true;
-//   }
-//   if (typeof radius === "undefined") {
-//     radius = 5;
-//   }
-//   if (typeof radius === "number") {
-//     radius = { tl: radius, tr: radius, br: radius, bl: radius };
-//   } else {
-//     const defaultRadius = { tl: 0, tr: 0, br: 0, bl: 0 };
-//     for (const side in defaultRadius) {
-//       radius[side] = radius[side] || defaultRadius[side];
-//     }
-//   }
-//   ctx.beginPath();
-//   ctx.moveTo(x + radius.tl, y);
-//   ctx.lineTo(x + width - radius.tr, y);
-//   ctx.quadraticCurveTo(x + width, y, x + width, y + radius.tr);
-//   ctx.lineTo(x + width, y + height - radius.br);
-//   ctx.quadraticCurveTo(x + width, y + height, x + width - radius.br, y + height);
-//   ctx.lineTo(x + radius.bl, y + height);
-//   ctx.quadraticCurveTo(x, y + height, x, y + height - radius.bl);
-//   ctx.lineTo(x, y + radius.tl);
-//   ctx.quadraticCurveTo(x, y, x + radius.tl, y);
-//   ctx.closePath();
-//   if (fill) {
-//     ctx.fill();
-//   }
-//   if (stroke) {
-//     ctx.stroke();
-//   }
-// }
+function roundRect(ctx, x, y, width, height, radius, fill, stroke) {
+  if (typeof stroke == "undefined") {
+    stroke = true;
+  }
+  if (typeof radius === "undefined") {
+    radius = 5;
+  }
+  if (typeof radius === "number") {
+    radius = { tl: radius, tr: radius, br: radius, bl: radius };
+  } else {
+    const defaultRadius = { tl: 0, tr: 0, br: 0, bl: 0 };
+    for (const side in defaultRadius) {
+      radius[side] = radius[side] || defaultRadius[side];
+    }
+  }
+  ctx.beginPath();
+  ctx.moveTo(x + radius.tl, y);
+  ctx.lineTo(x + width - radius.tr, y);
+  ctx.quadraticCurveTo(x + width, y, x + width, y + radius.tr);
+  ctx.lineTo(x + width, y + height - radius.br);
+  ctx.quadraticCurveTo(x + width, y + height, x + width - radius.br, y + height);
+  ctx.lineTo(x + radius.bl, y + height);
+  ctx.quadraticCurveTo(x, y + height, x, y + height - radius.bl);
+  ctx.lineTo(x, y + radius.tl);
+  ctx.quadraticCurveTo(x, y, x + radius.tl, y);
+  ctx.closePath();
+  if (fill) {
+    ctx.fill();
+  }
+  if (stroke) {
+    ctx.stroke();
+  }
+}
 ///////// PROCESS
 insertPlayers();
 console.log(dices);
