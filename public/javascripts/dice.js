93ee3e44769c8c4e54cf3f1e1d54b0fdd47002b6
lotiko <l.despres@netc.fr>
2021-04-09 13:57:48 +0200

    fix bug audio

--- public/javascripts/dice.js
+++ public/javascripts/dice.js
@@ -47,10 +47,10 @@ const massageCombi = {
 };
 const conboCombis = Object.keys(powerByComboCombi);
 const dicesSound = {
-  shot: new Audio("../audio/gobelet.mp3"),
-  roll3: new Audio("../audio/roll3Dice.mp3"),
-  roll2: new Audio("../audio/roll2dices.mp3"),
-  roll1: new Audio("../audio/roll1Dice.mp3"),
+  shot: new Audio("/audio/gobelet.mp3"),
+  roll3: new Audio("/audio/roll3Dice.mp3"),
+  roll2: new Audio("/audio/roll2dices.mp3"),
+  roll1: new Audio("/audio/roll1Dice.mp3"),
 };
 /**
  * Objet to create dice in the game
